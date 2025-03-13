679132764262c0056148b4c1198877f63aa8552b
Nicolas Santana <nicolassantanadev@gmail.com>
2025-03-13 16:56:08 -0300

    Updated 'sobre' section with new skills and expertise

--- script.js
+++ script.js
@@ -9,26 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.querySelector('.seta-esquerda').addEventListener('click', () => {
-    swiper.slidePrev();
-  });
-
-  document.querySelector('.seta-direita').addEventListener('click', () => {
-    swiper.slideNext();
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  
-  document.querySelector('.seta-esquerda').addEventListener('click', () => {
     swiper.slidePrev(); 
   });
 
@@ -36,17 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     swiper.slideNext(); 
   });
 
-  
   function smoothScroll(target, duration) {
     let startPosition = window.pageYOffset;
-    let targetPosition = document.getElementById(target).offsetTop - 50; 
+    let targetPosition = document.getElementById(target).offsetTop - 50; // Ajuste de 50px para o scroll
     let distance = targetPosition - startPosition;
     let startTime = null;
 
     function animation(currentTime) {
       if (startTime === null) startTime = currentTime;
       let timeElapsed = currentTime - startTime;
-      let progress = Math.min(timeElapsed / duration, 1); 
+      let progress = Math.min(timeElapsed / duration, 1);
 
       window.scrollTo(0, startPosition + distance * easeInOutCubic(progress));
 
@@ -62,12 +41,11 @@ document.addEventListener("DOMContentLoaded", function () {
     requestAnimationFrame(animation);
   }
 
-  
   document.querySelectorAll("nav a").forEach(anchor => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
-      smoothScroll(targetId, 800); 
+      smoothScroll(targetId, 800); // Usando scroll suave para qualquer link de navegação
     });
   });
 });
